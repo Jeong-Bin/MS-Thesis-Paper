cf786fa72763bcd87b1553d9929a4d1611445b3b
Hong Jeongbin <73174743+Jeong-Bin@users.noreply.github.com>
2023-08-07 11:22:51 +0900

    Add files via upload
    안 쓰는 코드 정리

--- mlkd_train.py
+++ mlkd_train.py
@@ -9,9 +9,9 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
 import timm
-from models.base_model import CNN4, CNN1
-from utils.util import seed_fixer, index_preprocessing, knowledge_distillation_loss
-from utils.CustomDatasets import make_df, CustomDataset, Meta_Transforms
+from models.BaseModels import CNN4, CNN1
+from utils import seed_fixer, index_preprocessing, knowledge_distillation_loss
+from data.DataPreprocessing import make_df, CustomDataset, Meta_Transforms
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -20,32 +20,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", default=2023, type=int)
-parser.add_argument("--dataset_name", default='mini_imagenet', type=str,
-                    choices=['MiniImagenet', 'mini_imagenet', 'mini-imagenet',
-                             'cub', 'CUB', 'CUB_200_2011',
-                            'TieredImagenet', 'tiered_imagenet', 'tiered-imagenet',
-                            'FC100', 'fc100', 'CIFARFS', 'cifarfs'])
-# parser.add_argument("--aug_mode", default='albumentations', type=str,
-#                     choices=['torchvision', 'transforms',
-#                              'albumentations', 'Albumentations', 'alb', 'Alb'])
+parser.add_argument("--dataset_name", default='CIFAR_FS', type=str,
+                    choices=['mini_imagenet', 'CUB_200_2011', 'CIFAR_FS'])
 
-parser.add_argument("--original_data_dir", default='/home/hjb3880/WORKPLACE/datasets/mini_imagenet', type=str)
-parser.add_argument("--corruption_data_dir", default='/home/hjb3880/WORKPLACE/datasets/mini_imagenet_C', type=str)
-parser.add_argument("--save_dir", default='saved_models_important2', type=str)
+parser.add_argument("--train_data_dir", default='/home/hjb3880/WORKPLACE/datasets/CIFAR_FS', type=str)
+parser.add_argument("--test_data_dir", default='/home/hjb3880/WORKPLACE/datasets/CIFAR_FS_C', type=str)
+parser.add_argument("--save_dir", default='saved_models_important_oc', type=str)
 
 parser.add_argument("--way", default=5, type=int)
-parser.add_argument("--train_shot", default=1, type=int)
-parser.add_argument("--train_query", default=1, type=int)
-parser.add_argument("--test_shot", default=1, type=int)
+parser.add_argument("--train_shot", default=10, type=int)
+parser.add_argument("--train_query", default=10, type=int)
+parser.add_argument("--test_shot", default=10, type=int)
 parser.add_argument("--test_query", default=15, type=int)
 
-parser.add_argument("--task_batch_size", default=12, type=int,
-                    help="Recommand: Use 12 when 1-shot and use 6 when 5&10-shot.")
+parser.add_argument("--task_batch_size", default=5, type=int,
+                    help="""Recommand: 10 and 5 for the 1-shot and 5&10-shot when using strong aug dataset.
+                            Recommand: 4 and 2 for the 1-shot and 5&10-shot when using clean dataset.
+                            """)
 parser.add_argument("--num_iterations", default=60000, type=int)
 
 parser.add_argument("--lr", default=1e-3, type=float)
-# parser.add_argument("--scheduler_step", default=30, type=int)
-parser.add_argument("--scheduler_gamma", default=0.5, type=float)
 
 parser.add_argument("--adapt_lr", default=0.01, type=float)
 parser.add_argument("--train_adapt_steps", default=5, type=int)
@@ -55,21 +49,16 @@ parser.add_argument("--meta_wrapping", default='maml', type=str)
 parser.add_argument("--first_order", default=True, type=bool)
 
 parser.add_argument("--kd_T", default=8, type=float)
-                                                                  # gpu1       gpu0
-parser.add_argument("--lambda_inner_ce", default=1, type=float) # 1  0.5       1    1    1  0.9  0.8
-parser.add_argument("--lambda_inner_kd", default=1, type=float) # 1  0.5     0.7  0.5  0.3  0.1  0.2
-parser.add_argument("--lambda_outer_ce", default=0.5, type=float)
-parser.add_argument("--lambda_outer_kd", default=0.5, type=float)
-
-# out  in |  1,1  |  0.9,0.1 |     0.5,0.5 |  1,0.3 |
-# 1,1     |    
-# 1,0.3   |        
-# 0.9,0.1 |  
-# 0.5,0.5 |
+parser.add_argument("--kd_mode", default='outer', type=str,
+                    choices=['inner', 'outer', 'dual'])
+                                            
+parser.add_argument("--lambda_inner_ce", default=1.0, type=float)
+parser.add_argument("--lambda_inner_kd", default=0.0, type=float)
+parser.add_argument("--lambda_outer_ce", default=1.0, type=float)
+parser.add_argument("--lambda_outer_kd", default=1.0, type=float)
 
 parser.add_argument("--teacher_backbone", default='convnext_small.in12k_ft_in1k_384', type=str,
-                    choices=['efficientnet_b1', 'mobilenetv3_largel_100', 'wide_resnet50_2', 'convnextv2_base.fcmae_ft_in22k_in1k_384', 'convnext_small.in12k_ft_in1k_384'])
-
+                    choices=['efficientnet_b1', 'wide_resnet50_2', 'convnext_small.in12k_ft_in1k_384'])
 
 parser.add_argument("--device", default='cuda', type=str)
 
@@ -77,8 +66,8 @@ args = parser.parse_args()
 
 config = {
         'argparse' : args,
-        'save_name_tag' : f'strong_CXs_2KD', ################################################
-        'readme' : 'valid_tasksets:-1'
+        'save_name_tag' : 'strong_CXs', ################################################
+        'memo' : """lr_scheduler 사용 안 함. 20번마다 validation."""
 }
 
 if args.device == 'cuda' :
@@ -89,19 +78,20 @@ elif args.device == 'cpu' :
 
 if args.dataset_name in ['MiniImagenet', 'mini_imagenet', 'mini-imagenet'] :
     dname = 'mini'
-elif args.dataset_name in ['cub', 'CUB', 'CUB_200_2011'] :
+elif args.dataset_name in ['cub', 'cub_200_2011', 'CUB', 'CUB_200_2011'] :
     dname = 'cub'
 elif args.dataset_name in ['TieredImagenet', 'tiered_imagenet', 'tiered-imagenet'] :
     dname = 'tiered'
 elif args.dataset_name in ['FC100', 'fc100'] :
     dname = 'fc100'
-elif args.dataset_name in ['CIFARFS', 'cifarfs'] :
-    dname = 'cifarfs'
+elif args.dataset_name in ['cifar_fs', 'cifar-fs', 'CIFAR_FS', 'CIFAR-FS'] :
+    dname = 'cifar'
 
-save_name = f"{dname}_{args.way}w{args.train_shot}s_{config['save_name_tag']}_{args.train_adapt_steps}step_in{int(args.lambda_inner_ce*10)}{int(args.lambda_inner_kd*10)}_out{int(args.lambda_outer_ce*10)}{int(args.lambda_outer_kd*10)}" #
+
+save_name = f"{dname}_{args.way}w{args.train_shot}s_{config['save_name_tag']}_{args.kd_mode}KD_{args.train_adapt_steps}step_in{int(args.lambda_inner_ce*10)}{int(args.lambda_inner_kd*10)}_out{int(args.lambda_outer_ce*10)}{int(args.lambda_outer_kd*10)}" #
 
 import wandb
-run = wandb.init(project="Train_OC")
+run = wandb.init(project="TRAIN_OC")
 wandb.run.name = save_name
 wandb.run.save()
 wandb.config.update(config)
@@ -113,7 +103,7 @@ def accuracy(predictions, targets):
 
 
 def fast_adapt(batch, adaptation_indices, evaluation_indices, 
-               teacher_backbone, teacher_learner, student_learner, #weight_learner,
+               teacher_backbone, teacher_learner, student_learner,
                criterion, adaptation_steps, mode, device):
     
     data, labels = batch
@@ -138,30 +128,28 @@ def fast_adapt(batch, adaptation_indices, evaluation_indices,
         student_adapt_logit = student_learner(adaptation_data)
         student_adapt_error = criterion(student_adapt_logit, adaptation_labels)
 
-        kd_loss = knowledge_distillation_loss(student_adapt_logit, teacher_adapt_logit, args.kd_T)
-        student_adapt_error = args.lambda_inner_ce*student_adapt_error + args.lambda_inner_kd*kd_loss
-
-        #student_adapt_error = weight_learner(student_adapt_error, kd_loss)
-
-        student_learner.adapt(student_adapt_error) #, allow_unused=True
-        # print('weight_learner:', list(weight_learner.parameters())[-1].grad)
-        # print('student_learner:', list(student_learner.parameters())[-1].grad)
+        if args.kd_mode in ['inner', 'dual']:
+            kd_loss = knowledge_distillation_loss(student_adapt_logit, teacher_adapt_logit, args.kd_T)
+            student_adapt_error = args.lambda_inner_ce*student_adapt_error + args.lambda_inner_kd*kd_loss
+        student_learner.adapt(student_adapt_error)
 
     teacher_eval_logit = teacher_learner(evaluation_feature)
-    teacher_evaluation_error = criterion(teacher_eval_logit, evaluation_labels)
-    teacher_evaluation_accuracy = accuracy(teacher_eval_logit, evaluation_labels)
+    teacher_eval_error = criterion(teacher_eval_logit, evaluation_labels)
+    teacher_eval_accuracy = accuracy(teacher_eval_logit, evaluation_labels)
 
     student_eval_logit = student_learner(evaluation_data)
-    student_evaluation_error = criterion(student_eval_logit, evaluation_labels)
-    if mode == 'train':
-        kd_loss = knowledge_distillation_loss(student_eval_logit, teacher_eval_logit, args.kd_T)
-        student_evaluation_error = args.lambda_outer_ce*student_evaluation_error + args.lambda_outer_kd*kd_loss
-    student_evaluation_accuracy = accuracy(student_eval_logit, evaluation_labels)
+    student_eval_error = criterion(student_eval_logit, evaluation_labels)
 
-    return teacher_evaluation_error, teacher_evaluation_accuracy , student_evaluation_error, student_evaluation_accuracy
+    if mode == 'train' and args.kd_mode in ['outer', 'dual']:
+        kd_loss = knowledge_distillation_loss(student_eval_logit, teacher_eval_logit, args.kd_T)
+        student_eval_error = args.lambda_outer_ce*student_eval_error + args.lambda_outer_kd*kd_loss
+    student_eval_accuracy = accuracy(student_eval_logit, evaluation_labels)
+
+    return teacher_eval_error, teacher_eval_accuracy , student_eval_error, student_eval_accuracy
     
 
 ################################## Train ##################################
+
 
 now = datetime.now()
 print(f"Start time: {now.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -170,10 +158,9 @@ def meta_train(args):
     seed_fixer(args.seed)
 
     train_data_transforms = A.Compose([
-        #A.Resize(84,84),
-
-        A.Resize(96,96),
-        A.CenterCrop(84,84),
+        # Don't use the Resize and CenterCrop when the dataset is CIFAR_FS.
+        # A.Resize(96,96),
+        # A.CenterCrop(84,84),
 
         A.OneOf([
                 A.GaussNoise(var_limit=(50.0, 500.0), p=1),
@@ -208,23 +195,23 @@ def meta_train(args):
     ])
 
     test_data_transforms = A.Compose([
+        # Already resized in the process of creating the C dataset.
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1),
         ToTensorV2()
     ])
 
-
-    trian = make_df(root=args.original_data_dir, mode='train')
-    train_dataset = CustomDataset(trian['img_path'].values, trian['label'].values, train_data_transforms)
     print('Making Train Tasksets...')
+    trian = make_df(root=args.train_data_dir, mode='train')
+    train_dataset = CustomDataset(trian['img_path'].values, trian['label'].values, train_data_transforms)
     train_tasksets = Meta_Transforms(dataset = train_dataset, 
                                     way = args.way, 
                                     shot = args.train_shot, 
                                     query = args.train_query, 
                                     num_tasks = -1)
     
-    valid = make_df(root=args.corruption_data_dir, mode='validation')
-    valid_dataset = CustomDataset(valid['img_path'].values, valid['label'].values, test_data_transforms)
     print('Making Valid Tasksets...')
+    valid = make_df(root=args.test_data_dir, mode='validation')
+    valid_dataset = CustomDataset(valid['img_path'].values, valid['label'].values, test_data_transforms)
     valid_tasksets = Meta_Transforms(dataset = valid_dataset, 
                                     way = args.way, 
                                     shot = args.test_shot, 
@@ -235,52 +222,48 @@ def meta_train(args):
     train_adapt_idx, train_eval_idx = index_preprocessing(way=args.way, shot=args.train_shot, query=args.train_query)
     test_adapt_idx, test_eval_idx = index_preprocessing(way=args.way, shot=args.test_shot, query=args.test_query)
 
+    if args.dataset_name in ['mini_imagenet', 'CUB_200_2011']:
+        img_sige = 84
+        hidden_dim = 64
+        spatial_size = 5
+    elif args.dataset_name == 'CIFAR_FS':
+        img_sige = 32
+        hidden_dim = 32
+        spatial_size = 2
 
     # maml Model
     teacher_backbone = timm.create_model(args.teacher_backbone, features_only=False, pretrained=True, num_classes=args.way)
-    a = teacher_backbone.forward_features(torch.randn(1,3,84,84))
+    a = teacher_backbone.forward_features(torch.randn(1,3,img_sige,img_sige))
     teacher_backbone.to(device)
 
     teacher = CNN1(channel_size=a.size(1), kernel_size=a.size(2), num_classes=args.way)
     teacher_maml = l2l.algorithms.MAML(teacher, lr=args.adapt_lr, first_order=False)
     teacher_maml.to(device)
 
-    student = CNN4(num_classes=args.way)
-    if args.meta_wrapping == 'maml' :
-        student_maml = l2l.algorithms.MAML(student, lr=args.adapt_lr, first_order=args.first_order)
-    elif args.meta_wrapping == 'metasgd' :
-        student_maml = l2l.algorithms.MetaSGD(student, lr=0.01, first_order=args.first_order)
-        args.train_adapt_steps = 1
-        args.test_adapt_steps = 1
+    student = CNN4(hidden_dim=hidden_dim, spatial_size=spatial_size, num_classes=args.way)
+    student_maml = l2l.algorithms.MAML(student, lr=args.adapt_lr, first_order=args.first_order)
     student_maml.to(device)
-
-    # metaweight = MetaWeights()
-    # weight_maml = l2l.algorithms.MAML(metaweight, lr=args.adapt_lr, first_order=False)
 
 
     criterion = nn.CrossEntropyLoss(reduction='mean')
 
     t_optimizer = optim.Adam(teacher_maml.parameters(), args.lr)
     s_optimizer = optim.Adam(student_maml.parameters(), args.lr)
-    # t_lr_scheduler = optim.lr_scheduler.StepLR(t_optimizer, step_size=args.scheduler_step, gamma=args.scheduler_gamma)
-    # s_lr_scheduler = optim.lr_scheduler.StepLR(s_optimizer, step_size=args.scheduler_step, gamma=args.scheduler_gamma)
-    t_lr_scheduler = optim.lr_scheduler.MultiStepLR(t_optimizer, milestones=[20000, 35000, 45000, 50000, 55000], gamma=args.scheduler_gamma)
-    s_lr_scheduler = optim.lr_scheduler.MultiStepLR(s_optimizer, milestones=[20000, 35000, 45000, 50000, 55000], gamma=args.scheduler_gamma)
 
     now = datetime.now()
     date = now.strftime('%m%d')
     best_teacher_accuracy = 0
     best_student_accuracy = 0
     for iteration in tqdm(range(1, args.num_iterations+1)):
-        train_teacher_loss_sum = 0.0
         train_teacher_accuracy_sum = 0.0
-        train_student_loss_sum = 0.0
+        train_teacher_loss_sum = 0.0
         train_student_accuracy_sum = 0.0
-
-        valid_teacher_loss_sum = 0.0
+        train_student_loss_sum = 0.0
+        
         valid_teacher_accuracy_sum = 0.0
-        valid_student_loss_sum = 0.0
+        valid_teacher_loss_sum = 0.0
         valid_student_accuracy_sum = 0.0
+        valid_student_loss_sum = 0.0
 
         t_optimizer.zero_grad()
         s_optimizer.zero_grad()
@@ -333,12 +316,10 @@ def meta_train(args):
         for p in teacher_maml.parameters():
             p.grad.data.mul_(1.0 / args.task_batch_size)
         t_optimizer.step()
-        t_lr_scheduler.step()
 
         for p in student_maml.parameters():
             p.grad.data.mul_(1.0 / args.task_batch_size)
         s_optimizer.step()
-        s_lr_scheduler.step()
 
 
         if iteration==1 or iteration%20==0:
@@ -379,6 +360,10 @@ def meta_train(args):
                 torch.save(student_maml.state_dict(), s_filepath)
                 print(f"✅ {iteration}: Best Student Model Saved. Valid Acc:{best_student_accuracy:.3f}%")
         
+
+        if iteration%5000==0:
+            now = datetime.now()
+            print(f"{iteration}: {now.strftime('%Y-%m-%d %H:%M:%S')}")
                 
 
 if __name__ == '__main__':
